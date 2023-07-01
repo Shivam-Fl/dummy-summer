9e01c339ca68de7bf80c54d2a176fa66457e8698
Shivam Mishra <shivam.r.mishra9120@gmail.com>
2023-07-01 10:46:41 +0530

    [CREATED]Hero section for OpenConf website created

---
+++ tailwind-Shivam_mishra/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Integral CF', 'Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
